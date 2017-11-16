c6bbf5a91740d7123de3edea1a18c46588d45d20
Chris Edmonds <Chris@chris-edmondss-macbook-pro.local>
2017-11-16 20:23:55 +1300

    Changed greeting

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 
 app.get('/', (rec, res) => {
-    res.send({ hi: 'there' });
+    res.send({ lmao: 'bub' });
 });
 
 const PORT = process.env.PORT || 5000;
